034dde7b3dcf026aa974a8f50f0d6f32670d7f71
Daniil Hovorun <daniil.hovorun@care.com>
2023-09-09 14:52:03 +0300

    UC#7 Infrastructure setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 plugins {
     id("java")
+    application
+    jacoco
+    id("org.sonarqube") version "4.2.1.3168"
 }
 
-group = "org.example"
+group = "uc7.unitTest"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -16,4 +19,32 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+application {
+    // Define the main class for the application.
+    mainClass.set("uc7.unitTest.Main")
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required = true
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Danhov_uc7-unit-test-ai")
+        property("sonar.organization", "danhov")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.token", "3e2efd7dd24072301286236f76e914eaa8359887")
+    }
 }
